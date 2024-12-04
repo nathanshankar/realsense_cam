b25939ae070f163cf3c49ee50b6ba15e4da7d962
Nathan Shankar <nathanshankar465@gmail.com>
2024-12-04 17:21:02 +0000

    modified:   launch/depth.launch.py

--- launch/depth.launch.py
+++ launch/depth.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
 
     # Specify the name of the package and path to xacro file within the package
     pkg_name = 'realsense_cam'
-    file_subpath = 'urdf/d435i_gz.urdf.xacro'
+    file_subpath = 'urdf/d435i.urdf.xacro'
 
 
     # Use xacro to process the file
